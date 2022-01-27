62ff8af634d7dca633d024a655a4ad3a444dc4b0
Andrew Morgan <andrew@amorgan.xyz>
2022-01-27 16:18:40 +0000

    Remove required space after command prefix

--- spec_todo_bot/config.py
+++ spec_todo_bot/config.py
@@ -103,7 +103,7 @@ class Config:
         )
         self.homeserver_url = self._get_cfg(["matrix", "homeserver_url"], required=True)
 
-        self.command_prefix = self._get_cfg(["command_prefix"], default="!c") + " "
+        self.command_prefix = self._get_cfg(["command_prefix"], default="!c")
 
     def _get_cfg(
         self,
